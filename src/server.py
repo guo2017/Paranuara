1e275b071fbbadf422657b053c03423d04bbfd37
Harry <harryzhang.sydney@gmail.com>
2018-03-22 16:58:16 +1100

    add 3 src files

---
+++ src/server.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, json, jsonify
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/')
+def index():
+    return "This is Parranuara"
+
+
+class Companies(Resource):
+    def get(self):
+        file_name = 'companies.json'
+        with open(file_name) as f:
+            json_data = json.load(f)
+            return json_data
+
+
+class People(Resource):
+    def get(self):
+        file_name = 'people.json'
+        with open(file_name) as f:
+            json_data = json.load(f)
+            print(type(json_data))
+            return json_data
+
+
+class CompanyEmployees(Resource):
+    def get(self, company_id):
+        file_name = 'people.json'
+        matched_employees = []
+        json_data = None
+        with open(file_name) as f:
+            json_data = json.load(f)
+
+        for employee in json_data:
+            if str(employee["company_id"]) == company_id:
+                matched_employees.append(employee)
+        if len(matched_employees) == 0:
+            return jsonify({"error": {"code": 204, "message": "no employees found"}})
+        else:
+            return jsonify(matched_employees)
+
+
+class CommonFriends(Resource):
+    def get(self, people01_id, people02_id):
+        file_name = 'people.json'
+        p1 = None
+        p2 = None
+        p1_friends = []
+        p2_friends = []
+        json_data = None
+        common_friends_ids = []
+        common_friends = []
+
+        with open(file_name) as f:
+            json_data = json.load(f)
+
+        for people in json_data:
+            if str(people["index"]) == people01_id:
+                p1 = {"name": people["name"], "age": people["age"], "address": people["address"],
+                      "phone": people["phone"]}
+                for x in people["friends"]:
+                    p1_friends.append(x["index"])
+
+            if str(people["index"]) == people02_id:
+                p2 = {"name": people["name"], "age": people["age"], "address": people["address"],
+                          "phone": people["phone"]}
+                for x in people["friends"]:
+                    p2_friends.append(x["index"])
+        common_friends_ids = [x for x in p1_friends if x in p2_friends]
+
+        for people in json_data:
+            if people["index"] in common_friends_ids and people["eyeColor"] == 'brown' and people['has_died'] == False:
+                common_friends.append(people)
+        result = {"people_one":p1, "people_two":p2, "common_friends":common_friends}
+
+        return jsonify(result)
+
+
+class FruitsVegetables(Resource):
+    def get(self, people_id):
+        fruit_list = ['apple', 'orange', 'banana', 'strawberry']
+        fruits = []
+        vegetables = []
+        target_people = None
+        json_data = None
+        file_name = "people.json"
+        with open(file_name) as f:
+            json_data = json.load(f)
+        for people in json_data:
+            if str(people["index"]) == people_id:
+                target_people = people
+        favorite_food = target_people["favouriteFood"]
+        fruits = [x for x in favorite_food if x in fruit_list]
+        vegetables = [x for x in favorite_food if x not in fruit_list]
+        result = {"username": target_people["name"], "age": str(target_people["age"]), "fruits": fruits, "vegetables": vegetables}
+
+        return jsonify(result)
+
+
+api.add_resource(Companies, '/companies')
+api.add_resource(People, '/people')
+api.add_resource(CompanyEmployees, '/employees/<company_id>')
+api.add_resource(CommonFriends, '/commonfriends/<people01_id>/<people02_id>')
+api.add_resource(FruitsVegetables, '/fruits/<people_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/test_server.py
@@ -0,0 +1,70 @@
+import unittest
+
+from flask import json
+
+from .server import app
+
+
+class ParanuarafTests(unittest.TestCase):
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+
+    def test_home_data(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the response data
+        self.assertEqual(result.data.decode('utf-8'), "This is Parranuara")
+
+    def test_compnay_employees(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        # if no matched employees, return error code 100
+
+        response = self.app.get('/employees/101')
+        json_data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(json_data['error']['code'], 204)
+
+        # assert the response data: there are employees, check the number of employees is right
+        response = self.app.get('/employees/1')
+        json_data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(len(json_data), 7)
+
+    def test_common_friends(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        # check if people with id 1 and 2 have common friends
+        response = self.app.get('/commonfriends/1/2')
+        json_data = json.loads(response.get_data(as_text=True))
+
+        # assert the response data, the number of common friends is 1 and its name is 'Decker Mckenzie'
+        self.assertEqual(len(json_data['common_friends']), 1)
+        self.assertEqual(json_data['common_friends'][0]['name'], 'Decker Mckenzie')
+
+    def test_fruits_vegetables(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        # check if people with id 2 return right name, age, fruits and vegerables
+        response = self.app.get('/fruits/2')
+        json_data = json.loads(response.get_data(as_text=True))
+
+        # assert the response data
+        self.assertEqual(json_data["age"], "54")
+        self.assertEqual(json_data['fruits'], ["orange", "banana", "strawberry"])
+        self.assertEqual(json_data['vegetables'], ["beetroot"])
+
